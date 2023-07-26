70cbd09ed61f420c52aa41b74533dcfa0a697435
Ekram Hossain <ekramhossain117@gmail.com>
2023-07-26 20:05:55 +0600

    positive numeber sums code file added

---
+++ Sum_Positive_number.js
@@ -0,0 +1,15 @@
+const  sumPositiveNumbers=(numbers)=> {
+      let sum = 0;
+      for (let i = 0; i < numbers.length; i++) {
+        if (numbers[i] > 0) {
+          sum += numbers[i];
+        }
+      }
+      return sum;
+    }
+    
+
+    const inputArray = [2, -5, 10, -3, 7];
+    const Sum = sumPositiveNumbers(inputArray);
+    console.log(Sum); 
+    
