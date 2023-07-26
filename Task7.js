f0569647e6d48ca64cf9caf1b87225eaaaf125aa
Ekram Hossain <ekramhossain117@gmail.com>
2023-07-26 23:30:02 +0600

    ROman to Integer

--- Task7.js
+++ Task7.js
@@ -1,7 +1,7 @@
 // Task 7: Implement a function that converts a Roman numeral to an integer. The function should take a Roman numeral string (e.g., "IX" or "XXI") as input and return the corresponding integer value.
 
 
-const romanToInt = function (s) {
+const romanToIntger = function (s) {
       let map = { "I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000 };
       let result = 0;
 
@@ -23,6 +23,6 @@ const romanToInt = function (s) {
 
 const romanNumeral2 = "XXI";
 
-const integerValue = romanToInt(romanNumeral2);
+const integerValue = romanToIntger(romanNumeral2);
 
 console.log(`The corresponding integer value of "${romanNumeral2}" is: ${integerValue}`);
